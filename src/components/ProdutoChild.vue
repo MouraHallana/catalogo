64b035c9e29c667028fff7331861f2ae20207d32
MouraHallana <hallana.victoria.moura@gmail.com>
2026-04-14 16:55:54 -0300

    aula dia 14/04

---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['nome', 'preco', 'categoria'])
+</script>
+<template>
+    <li>
+        {{ nome }} - R$ {{ preco }} ({{ categoria }})
+    </li>
+</template>
+<style scoped>
+</style>
